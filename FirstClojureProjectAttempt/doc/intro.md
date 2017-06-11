a92ff44653b87bda277da5b351fd44ff2a1969c1
petercsengodi <petercsengodi@users.noreply.github.com>
2017-06-11 11:28:21 +0200

    removed stupid clojure project attempt

--- FirstClojureProjectAttempt/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to first_clojure_attempt
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- FirstClojureProjectAttempt/test/firstclojureattempt/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns firstclojureattempt.core-test
-  (:require [clojure.test :refer :all]
-            [firstclojureattempt.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
