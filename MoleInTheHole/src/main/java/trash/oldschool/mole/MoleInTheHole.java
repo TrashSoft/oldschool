29e817bd9bd498595947ce48b15458c28f1c960e
petercsengodi <petercsengodi@users.noreply.github.com>
2017-06-11 08:17:43 +0200

    engine client skeleton

--- MoleInTheHole/src/main/java/trash/oldschool/mole/MoleInTheHole.java
+++ MoleInTheHole/src/main/java/trash/oldschool/mole/MoleInTheHole.java
@@ -3,6 +3,13 @@ package trash.oldschool.mole;
 import trash.oldschool.engine.GameAdapter;
 import trash.oldschool.engine.GameDescriptor;
 import trash.oldschool.engine.GameEngine;
+import trash.oldschool.engine.GameEngineStep;
+import trash.oldschool.mole.engine.MoleBuildStep;
+import trash.oldschool.mole.engine.MoleControlStep;
+import trash.oldschool.mole.engine.MoleDisposeStep;
+import trash.oldschool.mole.engine.MoleInitStep;
+import trash.oldschool.mole.engine.MoleModifyStep;
+import trash.oldschool.mole.engine.MoleRenderStep;
 import trash.oldschool.swing.SwingGameAdapter;
 
 public class MoleInTheHole {
@@ -16,8 +23,15 @@ public class MoleInTheHole {
 		descriptor.setDescription("Clone of an old C64 game.");
 
 		GameAdapter adapter = new SwingGameAdapter();
-
 		GameEngine engine = GameEngine.create(descriptor, adapter);
+		
+		engine.step(GameEngineStep.INIT, new MoleInitStep());
+		engine.step(GameEngineStep.BUILD, new MoleBuildStep());
+		engine.step(GameEngineStep.CONTROL, new MoleControlStep());
+		engine.step(GameEngineStep.MODIFY, new MoleModifyStep());
+		engine.step(GameEngineStep.RENDER, new MoleRenderStep());
+		engine.step(GameEngineStep.DISPOSE, new MoleDisposeStep());
+		
 		engine.startInNewWindow();
 	}
 
---
+++ MoleInTheHole/src/main/java/trash/oldschool/mole/engine/MoleDisposeStep.java
@@ -0,0 +1,14 @@
+package trash.oldschool.mole.engine;
+
+import trash.oldschool.engine.GameEngineCallback;
+import trash.oldschool.facade.Facade;
+
+public class MoleDisposeStep implements GameEngineCallback {
+
+	@Override
+	public Object call(Facade facade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
